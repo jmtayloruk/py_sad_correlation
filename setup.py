e7af561825e37f976ad736abed232aa4fbf65b97
JTLab <jtlab@JTLabs-Mac-mini.local>
2016-03-23 15:03:07 +0000

    Added further comments on path issues, and fixed case-sensitive filenames

--- setup.py
+++ setup.py
@@ -12,13 +12,17 @@ BUILD_MODULES=[]
 
 
 j_py_sad_correlation = Extension('j_py_sad_correlation',
-	include_dirs = ['/usr/local/include',
-                    '/Library/Python/2.6/site-packages/numpy/core/include/'],
-# This is a problem. On OS X I need to locally edit $PATH to remove anaconda because that installs python 3 and the code I have is not compatible with python 3!
+# Note that on my OS X machine the following is needed to work with the native python install
+# include_dirs = ['/usr/local/include',
+# '/Library/Python/2.6/site-packages/numpy/core/include/'],
+#
+# I also have problems with Anaconda having installed python 3. I need to locally edit $PATH to remove anaconda because that installs python 3 and the code I have is not compatible with python 3!
 # Need to work out a solution to this, and also a solution that enables me NOT to hard-code the path to the numpy header files
-#					'/Users/jonny/anaconda/lib/python3.5/site-packages/numpy/core/include/'],
-	library_dirs=['/usr/local/lib'],
-	sources = ['j_py_sad_correlation.cpp', 'common/JPythonArray.cpp', 'common/JPythonCommon.cpp', 'common/jAssert.cpp'],
+#
+# Anyway, the following works on the lab mac computer
+	include_dirs = ['/Users/jtlab/Anaconda/include','/Users/jtlab/Anaconda/pkgs/numpy-1.10.4-py27_0/lib/python2.7/site-packages/numpy/core/include'],
+	library_dirs=['/Users/jtlab/Anaconda/lib/'],
+	sources = ['j_py_sad_correlation.cpp', 'common/jPythonArray.cpp', 'common/jPythonCommon.cpp', 'common/jAssert.cpp', 'common/DebugPrintf_UNIX.cpp'],
 # Note: the -arch stuff below may be needed for OS X, but should be removed for Linux since it's a mac-specific parameter that is not recognised.
 	extra_link_args=['-arch', ARCH2],
 	extra_compile_args=['-O4', '-arch', ARCH2]
