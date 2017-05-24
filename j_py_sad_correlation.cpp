fe495ede6853ad1614cfbe781cc10b4159f0b6ff
Jonny Taylor <jonathan.taylor@glasgow.ac.uk>
2017-05-24 15:02:26 +0100

    Added function for doing SAD comparison of one frame with an array of reference frames

--- j_py_sad_correlation.cpp
+++ j_py_sad_correlation.cpp
@@ -14,6 +14,14 @@ template<class TYPE> void SetImageWindowForPythonWindow(ImageWindow<TYPE> &image
 	imageWindow.elementsPerRow = pythonWindow.Strides()[0];
 }
 
+template<class TYPE> void SetImageWindowForPythonWindow(ImageWindow<TYPE> &imageWindow, JPythonArray1D<TYPE> &pythonWindow)
+{
+	imageWindow.baseAddr = pythonWindow.Data();
+	imageWindow.width = pythonWindow.Dims()[0];
+	imageWindow.height = 1;
+	imageWindow.elementsPerRow = pythonWindow.Dims()[0];
+}
+
 template<int correlationType, class TYPE> void correlation3(JPythonArray2D<TYPE> &window1, JPythonArray2D<TYPE> &window2, JPythonArray2D<double> &result)
 {
 	// This conversion between types is a bit silly, but I now want to convert my python arrays to ImageWindow objects,
@@ -36,7 +44,7 @@ template<class TYPE> PyObject *correlation2(PyArrayObject *a, PyArrayObject *b, 
     JPythonArray2D<TYPE> window2(b);
     if (PyErr_Occurred()) return NULL;
     
-    if ((window1.NDims() != 2) || (window1.NDims() != 2))
+    if ((window1.NDims() != 2) || (window2.NDims() != 2))
     {
         PyErr_Format(PyErr_NewException((char*)"exceptions.TypeError", NULL, NULL), "Expected two 2D arrays as parameters");
         return NULL;
@@ -121,12 +129,77 @@ extern "C" PyObject *ssd_correlation(PyObject *self, PyObject *args)
 	return correlation(self, args, false);
 }
 
+extern "C" PyObject *sad_with_references(PyObject *self, PyObject *args)
+{
+	// Take the SAD between an image and a set of reference frames.
+	// Parameter 1: a 2D numpy array (MxN) representing a single MxN image.
+	// Parameter 2: a 3D numpy array (AxMxN) representing A separate MxN images.
+	// Result: a 2D numpy array (A) containing the results of the SAD comparisons between parameter 1 and each of the reference images
+
+	// parse the input arrays from *args
+	PyArrayObject *a, *b;
+	if (!PyArg_ParseTuple(args, "O!O!",
+						  &PyArray_Type, &a,
+						  &PyArray_Type, &b))
+	{
+		PyErr_Format(PyErr_NewException((char*)"exceptions.TypeError", NULL, NULL), "Unable to parse array!");
+		return NULL;
+	}
+	
+    JPythonArray2D<unsigned char> window1(a);
+    JPythonArray3D<unsigned char> refsWindow(b);
+    if (PyErr_Occurred()) return NULL;
+    
+    if ((window1.NDims() != 2) || (refsWindow.NDims() != 3))
+    {
+        PyErr_Format(PyErr_NewException((char*)"exceptions.TypeError", NULL, NULL), "Expected a 2D array and a 3D array as parameters");
+        return NULL;
+    }
+	
+	if ((PyArray_TYPE(a) == ArrayType<unsigned char>()) &&
+		(PyArray_TYPE(b) == ArrayType<unsigned char>()))
+	{
+		// Set up access to our source data
+		ImageWindow<unsigned char> imageWindow1;
+		SetImageWindowForPythonWindow(imageWindow1, window1);
+		
+		// Create a result array
+		npy_intp output_dims[1] = { refsWindow.Dims()[0] };
+		PyArrayObject *pythonResult = (PyArrayObject *)PyArray_SimpleNew(1, output_dims, NPY_DOUBLE);
+		JPythonArray1D<double> resultArray(pythonResult);
+		ImageWindow<double> resultWindow;
+		SetImageWindowForPythonWindow(resultWindow, resultArray);
+
+		// Iterate over the reference images, performing a comparison with each one
+		for (int i = 0; i < refsWindow.Dims()[0]; i++)
+		{
+			// Set up ImageWindows for our input and output arrays
+			ImageWindow<unsigned char> refsWindowEntry;
+			JPythonArray2D<unsigned char> temp = refsWindow[i];
+			SetImageWindowForPythonWindow(refsWindowEntry, temp);
+			ImageWindow<double> resultWindowEntry;
+			resultWindow.GetWindowOffset(resultWindowEntry, i, 0, 1, 1, 1, 1, 1, 1);
+			// Make use of the cross-correlation function that already exists for PIV
+			CrossCorrelateImageWindows<kCorrelationSAD>(imageWindow1, refsWindowEntry, resultWindowEntry);
+
+		}
+		
+		return PyArray_Return(pythonResult);
+	}
+    else
+    {
+		printf("Strides: %d %d\n", (int)PyArray_STRIDES(a)[0], (int)PyArray_STRIDES(a)[1]);
+        PyErr_Format(PyErr_NewException((char*)"exceptions.TypeError", NULL, NULL), "Unsuitable array types %d and %d passed in", PyArray_TYPE(a), PyArray_TYPE(b));
+        return NULL;
+    }
+}
 
 /* Define a methods table for the module */
 
 static PyMethodDef corr_methods[] = {
 	{"sad_correlation", sad_correlation, METH_VARARGS},	
-	{"ssd_correlation", ssd_correlation, METH_VARARGS},	
+	{"ssd_correlation", ssd_correlation, METH_VARARGS},
+	{"sad_with_references", sad_with_references, METH_VARARGS},
 	{NULL,NULL} };
 
 
